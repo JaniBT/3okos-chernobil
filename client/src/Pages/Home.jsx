0b4f6eb6d5e06fcb574cfded2e9605de7b02eb3c
János Csordás <janoscsordas072@gmail.com>
2024-04-30 19:09:06 +0200

    trying out stuff

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import './Home.css'
 
 const Home = () => {
+    const [isVisible, setIsVisible] = useState(true)
+    
 
+    const onAnimationEnd = () => {
+        setIsVisible(false)
+    }
 
     return (
         <>
+            <div className={`loading-animation ${isVisible ? 'visible' : 'nonVisible'}`} onAnimationEnd={onAnimationEnd}>
+                <h1>CHERNOBYL</h1>
+            </div>
             <h1>Placeholder</h1>
         </>
     )
