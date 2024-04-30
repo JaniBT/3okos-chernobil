5a7909ea569e3ac60ea31575e05fd2fe1edb42a2
János Csordás <janoscsordas072@gmail.com>
2024-04-30 18:17:27 +0200

    fixing some npm module imports
    and set up a basic routing for the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
