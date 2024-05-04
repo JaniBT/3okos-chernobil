1fd4953d17600bfeb10a7839fee4b4f7cf5da3ac
János Csordás <janoscsordas072@gmail.com>
2024-05-04 12:24:34 +0200

    finished home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import { IoIosArrowDown } from "react-icons/io";
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx"
 import hun_flag from '../assets/icons/hungarianIcon.png'
 import en_flag from '../assets/icons/englishIcon.png'
 import { useTranslation } from "react-i18next"
-import { useState } from 'react';
+import { useState } from 'react'
 
 const Navbar = () => {
     const [swap, setSwap] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
     const { t, i18n } = useTranslation()
 
     const handleLanguageIconSwap = () => {
@@ -22,20 +24,57 @@ const Navbar = () => {
         i18n.changeLanguage(language)
     }
 
+    const handleSidebarOpening = () => {
+        if (isOpen) {
+            closeSidebar()
+            setIsOpen(false)
+            return
+        }
+
+        openSideBar()
+        setIsOpen(true)
+    }
+
+    const openSideBar = () => {
+        document.querySelector('.hamburger-menu').style.transform = "translate3d(0%, 0%, 0px)"
+    }
+
+    const closeSidebar = () => {
+        document.querySelector('.hamburger-menu').style.transform = "translate3d(100%, 0%, -100px)"
+    }
+
     return (
         <header className='navigation-bar'>
             <h1>{t('heading')}</h1>
             <nav>
                 <Link to="/">{t('nav_one')}</Link>
-                <Link to="/">{t('nav_second')}</Link>
-                <Link to="/" title='Go to menus section'>
+                <Link to="/about">{t('nav_second')}</Link>
+                <Link to="/menus" title={t('moreMenuDesc')}>
                     <IoIosArrowDown /> 
                     {t('nav_third')}
                 </Link>
             </nav>
             <span>
-                <img src={`${!swap ? en_flag : hun_flag}`} onClick={handleLanguageIconSwap} width="32" height="32" alt="Language Changer" title='Click to change language' />
+                <img src={`${!swap ? en_flag : hun_flag}`} onClick={handleLanguageIconSwap} width="32" height="32" alt="Language Changer" title={t('languageSelectDesc')} />
             </span>
+            <div className='hamburger'>
+                <RxHamburgerMenu onClick={ handleSidebarOpening } />
+            </div>
+            <div id="hamburger-menu" className='hamburger-menu'>
+                <div className='hamburger-menu-title'>
+                    <h4>Menu</h4>
+                    <RxCross2 onClick={handleSidebarOpening} />
+                </div>
+                <div className='menus'>
+                    <Link to="/">{t('nav_one')}</Link>
+                    <Link to="/about">{t('nav_second')}</Link>
+                    <Link to="/menus" title={t('moreMenuDesc')}>
+                        <IoIosArrowDown /> 
+                        {t('nav_third')}
+                    </Link>
+                </div>
+                <button type="button" onClick={handleLanguageIconSwap}>{t('buttonText')}</button>
+            </div>
         </header>
     )
 }
