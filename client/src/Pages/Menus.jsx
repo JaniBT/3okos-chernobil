bc15e6b1530e129fb8cfcde6bca56badddc35fc1
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-05-06 17:58:45 +0200

    update-menus
    Created: MenuCards for components
    
    Updated: index.css -clr-grey, Menus.css and Menus,jsx

--- client/src/Pages/Menus.jsx
+++ client/src/Pages/Menus.jsx
@@ -1,17 +1,27 @@
-// import { useState } from "react";
-import { useTranslation } from "react-i18next";
+import React from "react"
+import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next"
+import MenuCard from "../components/MenuCard"
+import "./Menus.css"
 
 const Menus = () => {
+  const { t } = useTranslation()
 
-    const { t } = useTranslation();
-
-    return(
+  return (
     <>
-        <div className="menus-container">
-            <h1>{t("menusHeading")}</h1>
-        </div>
+      <div className="menus-container">
+        <header className="menus-home-link">
+          <nav>
+            <Link to="/">{t("nav_one")}</Link>
+          </nav>
+        </header>
+        <h1>{t("menusHeading")}</h1>
+        <p className="menusChoose">{t("menusChoose")}</p>
+        <MenuCard />
+        <p className="menuNote">{t("menuNote")}</p>
+      </div>
     </>
-    );
+  )
 }
 
 export default Menus
