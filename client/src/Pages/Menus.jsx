69e47791c9eb49ae8504b9942e18bbc33e576b46
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-05-07 10:45:19 +0200

    update-menus v2.
    TODO: Grid layout responsivity

--- client/src/Pages/Menus.jsx
+++ client/src/Pages/Menus.jsx
@@ -9,16 +9,25 @@ const Menus = () => {
 
   return (
     <>
-      <div className="menus-container">
-        <header className="menus-home-link">
-          <nav>
-            <Link to="/">{t("nav_one")}</Link>
-          </nav>
-        </header>
-        <h1>{t("menusHeading")}</h1>
-        <p className="menusChoose">{t("menusChoose")}</p>
-        <MenuCard />
-        <p className="menuNote">{t("menuNote")}</p>
+      <div className="menus-body">
+          <div className="menus-container">
+            <header className="menus-home-link">
+              <nav>
+                <Link to="/">{t("nav_one")}</Link>
+              </nav>
+            </header>
+            <h1>{t("menusHeading")}</h1>
+            <p className="menusChoose">{t("menusChoose")}</p>
+            <section className="menuSection">
+                <MenuCard link="/gallery" title={t("menuGallery")}/>
+                <MenuCard link="/stories" title={t("menuStories")}/>
+                <MenuCard link="/travel" title={t("menuTravel")}/>
+                <MenuCard link="/thoughts" title={t("menuThoughts")}/>
+                <MenuCard link="/places" title={t("menuPlaces")}/>
+                <MenuCard link="/hbo" title={t("menuHBO")}/>
+            </section>
+            <p className="menuNote">{t("menuNote")}</p>
+          </div>
       </div>
     </>
   )
--- client/src/components/MenuCard.jsx
+++ client/src/components/MenuCard.jsx
@@ -1,53 +1,19 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { useTranslation } from "react-i18next"
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-const Menu = () => {
-  const { t } = useTranslation()
+const Menu = (props) => {
+  const { t } = useTranslation();
 
   return (
-    <div>
-      <section className="menuSection">
-        <div className="menuCards">
-          <div className="menuCard">
-            <h2 className="cardLink">
-              <Link to="/">{t("menuGallery")}</Link>
-            </h2>
-          </div>
-          <div className="menuCard">
-            <h2 className="cardLink">
-              <Link to="/">{t("menuStories")}</Link>
-            </h2>
-          </div>
-          <div className="menuCard">
-            <h2 className="cardLink">
-              <Link to="/">{t("menuTravel")}</Link>
-            </h2>
-          </div>
+      <div className="menuCards">
+        <div className="menuCard">
+          <h2 className="cardLink">
+            <Link to={props.link}>{props.title}</Link>
+          </h2>
         </div>
-      </section>
+      </div>
+  );
+};
 
-      <section className="menuSection">
-        <div className="menuCards">
-          <div className="menuCard">
-            <h2 className="cardLink">
-              <Link to="/">{t("menuThoughts")}</Link>
-            </h2>
-          </div>
-          <div className="menuCard">
-            <h2 className="cardLink">
-              <Link to="/">{t("menuPlaces")}</Link>
-            </h2>
-          </div>
-          <div className="menuCard">
-            <h2 className="cardLink">
-              <Link to="/">{t("menuHBO")}</Link>
-            </h2>
-          </div>
-        </div>
-      </section>
-    </div>
-  )
-}
-
-export default Menu
+export default Menu;
