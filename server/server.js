9236f12837571e7bc78fa4efef5518e7327b50f0
JaniBT <janoscsordas072@gmail.com>
2024-05-07 08:17:33 +0200

    dev

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 
-const PORT = 8080
+const PORT = process.env.PORT
 
 const app = express()
+
+app.use('/api/users')
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`)
