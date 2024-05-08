877e370de810408d070cd38297bd45dd3b0bf847
JaniBT <janoscsordas072@gmail.com>
2024-05-08 11:19:51 +0200

    dev

--- client/src/components/GalleryHeroSection.jsx
+++ client/src/components/GalleryHeroSection.jsx
@@ -1,15 +1,19 @@
+import { useTranslation } from "react-i18next"
+import { BurningReactor } from "../assets/images/images"
 
 const GalleryHeroSection = () => {
+    const { t } = useTranslation()
 
     return (
         <>
             <div className="gallery-main-hero">
-                <div className="hero-text">
-                    <h1></h1>
+                <div className="hero-text hero-height">
+                    <h1>{t("GalleryMainTitle")}</h1>
                 </div>
-                <div className="hero-sm-picture">Szia</div>
-                <div className="hero-horizontal">Szia</div>
-                <div className="hero-vertical">Szia</div>
+                <div className="hero-sm-picture hero-height">
+                </div>
+                <div className="hero-horizontal hero-height">Szia</div>
+                <div className="hero-vertical hero-height">Szia</div>
             </div>
         </>
     )
