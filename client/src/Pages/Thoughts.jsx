44624a78aa93f5c4be89fbae2e9840f02547a261
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-05-08 09:09:10 +0200

    create pages
    Thoughts and other

---
+++ client/src/Pages/Thoughts.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+const Ideas = () => {
+    <Footer />
+}
+
+export default Ideas
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () =>{
+    <footer>
+        <p>{t("footerMain")}</p>
+        <p>{t("footerRights")}</p>
+    </footer>
+}
+
+export default Footer
