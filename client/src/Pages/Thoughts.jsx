a472ec4c3ebe4f9c4d92d6900264756492775d45
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-05-09 11:04:40 +0200

    update Thoughts page

--- client/src/Pages/Thoughts.jsx
+++ client/src/Pages/Thoughts.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import Footer from "../components/Footer";
+import { HiOutlinePaperAirplane } from "react-icons/hi";
 import "./Thoughts.css";
+import { Link } from "react-router-dom";
 
 const Thoughts = () => {
   const { t } = useTranslation();
 
+  const [message, setMessage] = useState("");
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+    setMessage("");
+  };
+
   return (
-    <>
-      <div className="thoughts-body">
-        <div className="thoughts-container">
-          <button>See all thoughts</button>
-          <p htmlFor="">
-            Tell us something about your ideas, thoughts, explain it in long
-            sentences: <span>*</span>
-          </p>
-          <form action="">
-            <label htmlFor=""></label>
-          </form>
-          <textarea name="" id="" placeholder="Write here..."></textarea>
-        </div>
+    <div className="thoughts-body">
+      <header className="inside-menus-wrapper">
+        <nav>
+          <Link to="/">{t("nav_one")}</Link>
+          <Link to="/menus">{t("menu_text")}</Link>
+        </nav>
+      </header>
+      <h1>Tell us something about your ideas, thoughts, explain it in long sentences: <span className="messageStar">*</span></h1>
+      <div className="thoughts-container">
+        <form onSubmit={handleSubmit}>
+          <textarea
+            className="messageArea"
+            placeholder="Write your thoughts here..."
+            value={message}
+            onChange={handleMessageChange}
+          ></textarea>{" "}
+          <br />
+          <button className="messageBtn" type="submit">
+            <HiOutlinePaperAirplane /> {t("thoughtsMessage")}
+          </button>
+          <button className="revealBtn" type="button">
+            <HiOutlinePaperAirplane /> {t("thoughtsMessage")}
+          </button>
+        </form>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 };
 
