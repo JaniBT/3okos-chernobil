78f514766aca5cdc23a51523d0fd810ac5b238ca
arris05 <andrasvasari47@gmail.com>
2024-05-10 12:45:14 +0200

    finishing about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -18,13 +18,13 @@ const About = () => {
               <Link to="/">{t('nav_one')}</Link>
             </nav>
           </header>
-          <h1>{t("About us")}</h1>
-          <iframe width="822" height="515" src="https://www.youtube.com/embed/f5ptI6Pi3GA?si=pLR6y4jb7G3oBO8W" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-          <p>{t("In memorial of the chernobyl catastrophe.")}</p>
+          <h1>{t("aboutUs")}</h1>
+          <iframe src="https://www.youtube.com/embed/f5ptI6Pi3GA?si=pLR6y4jb7G3oBO8W" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <p>{t("iFrameLabel")}</p>
           <hr className="first-hr" />
-          <h2>{t("This page was made in memorial of the people who suffered through these times.")}</h2>
+          <h2>{t("inMemorial")}</h2>
           <HiArrowNarrowDown />
-          <h3>{t("view gallery")}</h3>
+          <h3>{t("gallery")}</h3>
           <div className="about-images">
             <AboutImages source={ControlRoom} />
             <AboutImages source={BurningReactor} />
@@ -33,24 +33,24 @@ const About = () => {
             <AboutImages source={Liquidators} />
             <AboutImages source={OldSarcophage} />
           </div>
-          <Link className="learn-more-link" to="https://en.wikipedia.org/wiki/Chernobyl_disaster">learn more</Link>
+          <Link className="learn-more-link" to="https://en.wikipedia.org/wiki/Chernobyl_disaster">{t("moreInfo")}</Link>
           <div className="curiosity-texts">
             <div className="left-text">
-              <p>{t("Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus similique quisquam architecto. Harum, ad consequatur aut obcaecati, culpa fugiat eius quidem amet, quam cumque magni.")}</p>
-              <p>{t("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi nobis, dolore adipisci officiis ducimus nihil. Harum ut, nulla iusto rem aliquid voluptatibus tenetur modi natus excepturi beatae quam obcaecati ab ex? Alias dolore atque inventore voluptas officiis doloremque dolores dolorum, laudantium sequi, itaque voluptatibus? Sunt beatae et sed quia deserunt?")}</p>
+              <p>{t("moreInfoTextFirst")}</p>
+              <p>{t("moreInfoTextSecond")}</p>
             </div>
             <div className="right-text">
-              <p>{t("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi nobis, dolore adipisci officiis ducimus nihil. Harum ut, nulla iusto rem aliquid voluptatibus tenetur modi natus excepturi beatae quam obcaecati ab ex? Alias dolore atque inventore voluptas officiis doloremque dolores dolorum, laudantium sequi, itaque voluptatibus? Sunt beatae et sed quia deserunt?")}</p>
+              <p>{t("moreInfoTextThird")}</p>
             </div>
           </div>
-          <h4>{t("We are the 3okos team")}</h4>
+          <h4>{t("teamInfo")}</h4>
           <div className="team-members">
             <AboutTeamMembers source={JaniKep} name={t("Csordás János")} link_github={"https://github.com/JaniBT"} link_name_github={"Github"} link_instagram={"https://www.instagram.com/_jano666_/"} link_name_instagram={"Instagram"} />
             <AboutTeamMembers source={BalazsKep} name={t("Koós Balázs Bence")} link_github={"https://github.com/koosbalazsbence"} link_name_github={"Github"} link_instagram={"https://www.instagram.com/koosbalazsbence/"} link_name_instagram={"Instagram"} />
             <AboutTeamMembers source={AndrisKep} name={t("Vásári András")} link_github={"https://github.com/arris05"} link_name_github={"Github"} link_instagram={"https://www.instagram.com/andris_vsri_/"} link_name_instagram={"Instagram"} />
           </div>
           <hr className="second-hr" />
-          <h6>{t("You can contact us at any of our available social media accounts.")}</h6>
+          <h6>{t("contactInfo")}</h6>
         </div>
         <Footer />
       </div>
--- client/src/components/AboutTeamMembers.jsx
+++ client/src/components/AboutTeamMembers.jsx
@@ -6,8 +6,6 @@ const AboutTeamMembers = (props) => {
             <div className="team-member">
                 <img className="tm-profile-picture" src={props.source} alt={props.alternative} />
                 <h5 className="tm-name">{props.name}</h5>
-                <ul className="tm-jobs">
-                </ul>
                 <div className="tm-links">
                     <Link to={props.link_github} target="_blank">{props.link_name_github}</Link>
                     <Link to={props.link_instagram} target="_blank">{props.link_name_instagram}</Link>
